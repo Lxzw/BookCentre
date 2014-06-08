12c172812577b11fa22cfbead56efc6f2543cbac
L-win8 <woaishisi@126.com>
2014-06-08 23:40:12 +0800

    SPRINGMVC的框架能够正常运行

--- src/main/java/com/l/spring/mvc/controller/HomeController.java
+++ src/main/java/com/l/spring/mvc/controller/HomeController.java
@@ -1,10 +1,23 @@
 package com.l.spring.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.l.spring.dao.UserDao;
+
 @Controller
 public class HomeController {
+	
+	private UserDao userDao;
+	/**
+	 * 通过构造器注入userDao
+	 * @param userDao
+	 */
+	@Autowired
+	public HomeController(UserDao userDao) {
+		this.userDao = userDao;
+	}
 	
 	@RequestMapping({"/","home"})
 	public String showHomePage() {
